3753796b6d062158bd338598205ce444b6124230
Logan <logan.dewilfond@gmail.com>
2018-12-10 13:20:35 -0600

    yes

---
+++ RentPrice/RentPrice/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace RentPrice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtLocation = new System.Windows.Forms.TextBox();
+            this.txtRooms = new System.Windows.Forms.TextBox();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(126, 162);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtLocation
+            // 
+            this.txtLocation.Location = new System.Drawing.Point(117, 45);
+            this.txtLocation.Name = "txtLocation";
+            this.txtLocation.Size = new System.Drawing.Size(100, 20);
+            this.txtLocation.TabIndex = 1;
+            // 
+            // txtRooms
+            // 
+            this.txtRooms.Location = new System.Drawing.Point(117, 115);
+            this.txtRooms.Name = "txtRooms";
+            this.txtRooms.Size = new System.Drawing.Size(100, 20);
+            this.txtRooms.TabIndex = 2;
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblOutput.Location = new System.Drawing.Point(80, 210);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(161, 44);
+            this.lblOutput.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Floor Location";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Number of Rooms";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(126, 278);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(321, 398);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.txtRooms);
+            this.Controls.Add(this.txtLocation);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "RentPrices";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtLocation;
+        private System.Windows.Forms.TextBox txtRooms;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Saving into an array/Saving into an array/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Saving_into_an_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice;
+            int[] arr = new int[10];
+            int i;
+            Write("\n\nRead and print elements of an array:\n");
+            Write("-----------------------------------------\n");
+            Write("Input 10 elements in the array");
+            for (i = 0; i< 10; i++)
+            {
+                Write("Enter number - " + i + " << ");
+                arr[i] = Convert.ToInt32(ReadLine());
+            }
+            WriteLine("Choose display");
+            choice = Convert.ToInt32(ReadLine());
+            if (choice == 1)
+            {
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    WriteLine("Numbers entered were " + arr[j]);
+                }
+            }
+            else if (choice == 2)
+            {
+                for (int j = 8; j >= 0; j--)
+                {
+                    WriteLine("Numbers entered backwards were " + arr[j]);
+                }
+            }
+            ReadLine();
+        }
+    }
+}
