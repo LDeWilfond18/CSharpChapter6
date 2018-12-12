407c6aea7aaf0946f536558c184b328596bc0882
Logan <logan.dewilfond@gmail.com>
2018-12-12 12:48:58 -0600

    yeet

--- CheckZips/CheckZips/Program.cs
+++ CheckZips/CheckZips/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace CheckZips
 {
@@ -10,6 +11,23 @@ namespace CheckZips
     {
         static void Main(string[] args)
         {
+            int userZip;
+            int i = 0;
+            int[] zips = { 11111, 22222, 33333, 44444, 55555, 66666, 77777, 88888, 99999, 52804 };
+
+            Write("Enter the zip code of the package");
+            userZip = Convert.ToInt32(ReadLine());
+
+            while(userZip != zips[i])
+            {
+                i++;
+                if (userZip == zips[i])
+                {
+                    WriteLine("Package is in the right trailer");
+                }
+                else
+                    WriteLine("Misflow, do not load");
+            }
         }
     }
 }
---
+++ ResortPrices/ResortPrices/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ResortPrices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days;
+            int[] nightPrices = { 0, 200, 200, 180, 180, 160, 160, 160, 145 };
+            Write("Welcome to the Carefree Resort. How many nights?");
+            days = Convert.ToInt32(ReadLine());
+            if(days >= 8)
+            {
+                WriteLine("The cost will be " + nightPrices[days]);
+            }
+            else if (days > 8)
+            {
+                WriteLine("Error");
+            }
+
+        }
+    }
+}
